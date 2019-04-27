b3d66a8ee8dedeab5528ea43df430182943169c4
Anais Trope <trope.anais@gmail.com>
2019-04-27 22:10:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Add a and b and returns the result. 
 function add(a, b){
-  return a + b
+  return (a + b)
 }
 
 // Substract a and b and returns the result.
 function substract(a, b){
-  return a - b
+  return (a - b)
 }
