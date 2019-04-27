8e309ae0eb284183019ff0264d2eea47fd4d2a50
Anais Trope <trope.anais@gmail.com>
2019-04-27 22:12:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-// Add a and b and returns the result. 
+// Adds a and b and returns the result. 
 function add(a, b){
   return (a + b)
 }
 
-// Subtract a and b and returns the result.
+// Subtracts a and b and returns the result.
 function subtract(a, b){
   return (a - b)
 }
+
+// Multiplies a by b and returns the result.
+function multiply(a, b){
+  return (a * b)
+}
+
+// Divides a and b and returns the result.
+function divide(a, b){
+  return (a / b)
